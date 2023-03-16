2a5389e9310d2154ab292eff5b8bf4aace2ac2b8
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-03-16 09:28:45 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Prints a message to the console
+ * Return: 0 (Success)
+ */
 int main(void)
 {
+/*Puts is designed to print strings to the console*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
