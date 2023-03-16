a7c2330e6a8c07876672005df41f2d870cc40fd7
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-03-16 10:02:26 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a message to the console
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+/*Printf is designed to print stringsand variabls to the console*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
