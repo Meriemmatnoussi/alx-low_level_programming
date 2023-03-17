85b805f3727f4f1d475605a62f3bc5d7dd99d68b
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-03-17 13:28:07 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdbool.h>
 
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-
 
 int main(void)
 {
-	int i, j;
+	int i, j, k, l;
+	bool is_first_iteration = true;
 
-	for (i = 0; i < 100; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i; j < 100; j++)
+		for (j = 0; j < 10; j++)
 		{
-			int a = i / 10, b = i % 10;
-			int c = j / 10, d = j % 10;
-
-			putchar('0' + a);
-			putchar('0' + b);
-			putchar(' ');
-			putchar('0' + c);
-			putchar('0' + d);
-
-			if (i != 99 || j != 99)
+			for (k = i; k < 10; k++)
 			{
-				putchar(',');
-				putchar(' ');
+				for (l = 0; l < 10; l++)
+				{
+					if (i == k && l <= j)
+					{
+						continue;
+					}
+
+					if (is_first_iteration == false)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+
+					is_first_iteration = false;
+				}
 			}
 		}
 	}
 
+	putchar('\n');
 	return (0);
 }
