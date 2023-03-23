048c6b5647f355c89fc932021f6fce23b634d3a8
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-03-23 08:49:21 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+
+	else
+		return (0);
+}
