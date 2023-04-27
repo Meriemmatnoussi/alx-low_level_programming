5e59cf2c78776921027e8b30bc56ca68fcf709e6
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-04-27 06:29:43 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * before_main - A function that prints a message before the main function
+ *
+ * Return: void
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
