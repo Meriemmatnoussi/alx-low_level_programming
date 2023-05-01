90617d8a0a574dbe6cf2879a2d49b54a9294823c
Meriemmatnoussi <meriem.matnoussi123@gmail.com>
2023-05-01 05:03:42 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,33 +6,72 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to head of list
+ * print_listint_safe - Prints a listint_t linked list.
+ *                      Can handle linked lists with a loop.
  *
- * Return: the number of nodes in the list
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current, *loop_start;
-	size_t i = 0, j;
+	size_t count = 0;
+	const listint_t *slow, *fast;
 
-	current = head;
-	while (current != NULL)
+	if (head == NULL)
+		exit(98);
+
+	slow = head;
+	fast = head;
+
+	while (fast != NULL && fast->next != NULL)
 	{
-		loop_start = current;
-		for (j = 0; j < i; j++)
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
 		{
-			if (loop_start == current)
-			{
-				printf("-> [%p] %d\n", (void *)current, current->n);
-				exit(98);
-			}
-			loop_start = loop_start->next;
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			count++;
+
+		/* Detect a loop */
+			break;
 		}
-		printf("[%p] %d\n", (void *)current, current->n);
-		i++;
-		current = current->next;
 	}
 
-	return (i);
+	/* If the loop has been detected */
+	if (slow == fast)
+	{
+		slow = head;
+
+		while (slow != fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			count++;
+
+			slow = slow->next;
+			fast = fast->next;
+		}
+
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+
+		printf("-> [%p] %d\n", (void *)fast, fast->n);
+	}
+	/* If there is no loop */
+	else
+	{
+		while (slow != NULL)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			count++;
+
+			slow = slow->next;
+		}
+	}
+
+	return (count);
 }
